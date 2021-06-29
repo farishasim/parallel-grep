f144954318b9005b934a8894d0230ffbe7b0ffb6
Faris Hasim <farishasimsyauqi@gmail.com>
2021-06-29 14:52:31 +0700

    update parent

--- openmp/grep.c
+++ openmp/grep.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
+#include<sys/wait.h>
 
 void test(int ID, int pid);
 
@@ -29,6 +30,7 @@ int main(int argc, char ** args) {
     //*** PARENT SECTION ***//
     int i = 0;
     while(i < NPROC) {
+        // parent process fork child process
         if ((cpids[0] = fork()) == 0) {
             // child process jump to child section
             goto child;
@@ -38,7 +40,9 @@ int main(int argc, char ** args) {
         }
     }
 
-    // parent process exit
+    // parent process exit when all child terminated
+    while(wait(NULL) > 0);
+    printf("Parent process ended\n");
     exit(0);
 
     //*** CHILD SECTION ***//
