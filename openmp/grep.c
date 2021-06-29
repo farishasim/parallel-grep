a740012bbbb404a697fcdf93bf2d465d8a80d9ad
Faris Hasim <farishasimsyauqi@gmail.com>
2021-06-29 14:46:20 +0700

    add general scheme

--- openmp/grep.c
+++ openmp/grep.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <dirent.h>
+
+void test(int ID, int pid);
 
 int main(int argc, char ** args) {
 
@@ -9,6 +12,7 @@ int main(int argc, char ** args) {
     int NPROC;
     int NTHRD;
     char * STR;
+    DIR * dir;
     
     if (argc < 4) {
         printf("Usage : ./grep n-process n-thread string\n");
@@ -22,19 +26,38 @@ int main(int argc, char ** args) {
 
     cpids = (pid_t*) malloc ( sizeof(pid_t) * NPROC );
 
-    // if ((cpid = fork()) == 0) {
-    //     printf("Hello from child");
-    // } else {
-    //     printf("Hello from parent");
-    // }
+    //*** PARENT SECTION ***//
+    int i = 0;
+    while(i < NPROC) {
+        if ((cpids[0] = fork()) == 0) {
+            // child process jump to child section
+            goto child;
+        } else {
+            // parent process continue forking another child
+            i++;
+        }
+    }
 
+    // parent process exit
+    exit(0);
+
+    //*** CHILD SECTION ***//
+    child:
     omp_set_num_threads(NTHRD);
     #pragma omp parallel
     {
     int ID = omp_get_thread_num();
-    printf("hello(%d)", ID);
-    printf(" world(%d)\n", ID);
+    // printf("hello(%d)", ID);
+    // printf(" world(%d)\n", ID);
+    test(ID, getpid());
     }
     
     return 0;
+}
+
+void test(int ID, int pid) {
+    int ano = 1;
+    printf("hello(%d, %d)", ID, pid);
+    printf(" world(%d, %d)\n", ID, pid);
+    return;
 }
