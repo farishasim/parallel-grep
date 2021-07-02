420040257e35837ebd9ddb55855d6e7a85d55932
Faris Hasim <farishasimsyauqi@gmail.com>
2021-07-02 09:55:04 +0700

    again, change general sheme

--- openmp/grep.c
+++ openmp/grep.c
@@ -4,13 +4,71 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 
 void test(int ID);
 
 #define BLOCK 4096
+#define NORMAL_COLOR  "\x1B[0m"
+#define GREEN  "\x1B[32m"
+#define BLUE  "\x1B[34m"
 
 int NPROC;
 int NTHRD;
+
+// List definition
+typedef int fd;
+typedef struct tNode* ptr;
+typedef struct tNode {
+    fd info;    // file descriptor
+    ptr next;   // next pointer
+} Node ;
+typedef ptr List;
+
+ptr AlokNode(fd info) {
+    ptr P = (ptr) malloc (sizeof(Node));
+    P->info = info;
+    P->next = NULL;
+    return P;
+}
+
+ptr last(List L) {
+    ptr P = L;
+    while (P->next) P = P->next;
+    return P;
+}
+
+List listing(char * path) {
+    List L = NULL; ptr P = AlokNode(0); // dummy node
+    DIR * d = opendir(path);
+    if (d==NULL) return L;
+    struct dirent * dir;
+    char d_path[512];
+    while ((dir = readdir(d)) != NULL) {
+        sprintf(d_path, "%s/%s", path, dir->d_name);
+        printf("%s\n", d_path);
+        if (dir->d_type != DT_DIR) // for files
+        {
+            P->next = AlokNode(fileno(fopen(d_path, "r")));
+            P = P->next;
+            if (!L) L = P;
+        }
+        else if ((dir->d_type == DT_DIR) // for directory
+                && (strcmp(dir->d_name,".") != 0) 
+                && (strcmp(dir->d_name,"..") != 0))
+        {
+            P->next = listing(d_path);
+            if (P->next){
+                P = last(P->next);
+                if (!L) L = P->next;
+            }
+        }
+    }
+    closedir(d);
+    return L;
+}
 
 int main(int argc, char ** args) {
 
@@ -24,21 +82,15 @@ int main(int argc, char ** args) {
         exit(0);
     }
 
+    List L = listing(".");
+
     //*** INIT SECTION ***//    
     NPROC = atoi(args[1]);
     NTHRD = atoi(args[2]);
     STR = args[3];
 
     // parallelism for file search
-    omp_set_nested(1);
-    omp_set_num_threads(NPROC);
-    #pragma omp parallel 
-    {
-        int ID = omp_get_thread_num();
-        printf("this is from process(%d): ", ID);
 
-        test(ID);
-    }
 }
 
 void test(int ID) {
